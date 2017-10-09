6eb2e73f71217a79aa9fee2450e0eff44ef914f3
Sharanappa Golageri, Mallappa <Mallappa.SharanappaGolageri@cdk.com>
2017-10-09 15:12:54 +0530

    second largest number

---
+++ src/main/java/test/SecondLargestNumber.java
@@ -0,0 +1,26 @@
+package test;
+
+/**
+ * Created by golagem on 10/9/17.
+ */
+public class SecondLargestNumber {
+
+    public static void main(String... s){
+        int[] a={2,2,1};
+        int first=a[0];
+        int second=0;
+        for(int i=0;i<a.length;i++){
+            if(first<a[i]){
+                second=first;
+                first=a[i];
+            }
+            else             {
+                if(second<a[i] && a[i]<first){
+                    second=a[i];
+                }
+            }
+        }
+
+        System.out.println(second);
+    }
+}
